c0851a2367fa27bd03a228c1a6aee44413401e3e
Michelle Hey <48541701+HeyMichelle@users.noreply.github.com>
2021-03-01 18:10:46 -0500

    Testing react applications (#1)
    * remove reset function
    
    * Update packages
    
    * Update README.md
    
    * initial testing completed
    
    * Imported screen from testing library to app.test.js
    
    * Some testing added, full functionality not completed.
    
    * added testing.
    
    * tests successfully ran
    
    Co-authored-by: Dustin Myers <dustin@lambdaschool.com>
    Co-authored-by: Dustin Myers <dustinmyers@users.noreply.github.com>

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,10 +3,10 @@ import { useForm } from "react-hook-form";
 
 const ContactForm = () => {
   const [data, setData] = useState();
-  const { register, errors, handleSubmit, reset } = useForm({
-    mode: "onBlur"
+  const { register, errors, handleSubmit } = useForm({
+    mode: "onBlur",
   });
-  const onSubmit = data => {
+  const onSubmit = (data) => {
     setData(data);
   };
 
@@ -17,7 +17,8 @@ const ContactForm = () => {
           <label htmlFor="firstName">First Name*</label>
           <input
             name="firstName"
-            placeholder="bill"
+            placeholder="Edd"
+            data-testid="firstNameInput"
             ref={register({ required: true, maxLength: 3 })}
           />
           {errors.firstName && (
@@ -29,7 +30,8 @@ const ContactForm = () => {
           <label htmlFor="lastName">Last Name*</label>
           <input
             name="lastName"
-            placeholder="luo"
+            placeholder="Burke"
+            data-testid="lastNameInput"
             ref={register({ required: true })}
           />
           {errors.lastName && (
@@ -38,7 +40,8 @@ const ContactForm = () => {
         </div>
 
         <div>
-          <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
+          <label htmlFor="email"
+          data-testid="emailInput" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
           <input name="email" ref={register({ required: true })} />
@@ -48,7 +51,7 @@ const ContactForm = () => {
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <textarea name="message" data-testid="textInput" ref={register({ required: false })} />
         </div>
         {data && (
           <pre style={{ textAlign: "left", color: "white" }}>
